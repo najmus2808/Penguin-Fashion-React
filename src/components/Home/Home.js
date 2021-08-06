a100db88ebe42b0e2d13e8aa55010c5aeaeff93b
najmus2808 <najmus2808@gmail.com>
2021-08-06 11:28:07 +0600

    Navbar done!

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../Button/Button'
+import Navbar from '../Navbar/Navbar'
+
+function Home() {
+    return (
+        <div>
+            <Navbar/>
+            <Button/>
+        </div>
+    )
+}
+
+export default Home
