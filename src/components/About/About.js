49a798acc68250ea85ac03f66d620d4294506e4a
najmus2808 <najmus2808@gmail.com>
2021-08-07 23:34:22 +0600

    About Us section mostly done!

---
+++ src/components/About/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import image from "../../images/XMLID 1.png";
+import "./About.css"
+
+function About() {
+  return (
+    <section className="container about">
+      <div className="row">
+        <div className="col-lg-6 col-12 p-5">
+          <h2 className="text-center">About Us</h2>
+          <p className="">
+            Lorem ipsum dolor sit amet, consectetur adip. Lorem ipsum dolor sit
+            amet, consectetur adip. Lorem ipsum dolor sit amet, consectetur
+            adip. Lorem ipsum dolor sit amet, consectetur adip. Lorem ipsum
+            dolor sit amet, consectetur adip. Lorem ipsum dolor sit amet,
+            consectetur adip. Lorem ipsum dolor sit amet, consectetur adip.
+            Lorem ipsum dolor.
+          </p>
+          <button type="button" className="btn btn-primary">
+            See More
+          </button>
+        </div>
+        <div className="col-lg-6 col-12 p-5">
+          <img style={{ width: "100%" }} src={image} alt="" className="img-fluid" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,16 @@ import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import CasualShoes from "../CasualShoes/CasualShoes";
 import FormalShoes from "../FormalShoes/FormalShoes";
+import About from "../About/About";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Header />
-      <CasualShoes/>
-      <FormalShoes/>
-      
+      <CasualShoes />
+      <FormalShoes />
+      <About />
     </div>
   );
 }
